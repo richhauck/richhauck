e3f107fc4a1ce98e18acfc941dcb7bf877496c45
Rich Hauck <97491179+rdhauck@users.noreply.github.com>
2022-04-27 15:54:13 -0400

    Random photo displays now on homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,33 @@
-import type { Component } from "solid-js";
+import { Component, createEffect } from "solid-js";
+import photosResource from "../stores/photosResource";
 const Home: Component = () => {
+  const { photos, getRandomPhoto } = photosResource;
+
+  createEffect(() => {
+    if (photos()) {
+      console.log("random", getRandomPhoto().src);
+      const randomImgSrc = getRandomPhoto().src;
+      document
+        .getElementById("bg")
+        .setAttribute(
+          "style",
+          `background:url(${randomImgSrc}) no-repeat; background-size:cover;`
+        );
+    }
+  });
+
   return (
-    <div class="h-full absolute w-80 h-12 bg-black">
-      <h1>Designer.Engineer.Teacher.Artist.</h1>
+    <div>
+      <div id="bg"></div>
+      <div class="relative h-96">
+        <div class="absolute bg-black -right-0 top-2/4 p-12">
+          <h1>
+            Designer.Engineer.
+            <br />
+            Teacher.Artist.
+          </h1>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Photos.tsx
+++ src/pages/Photos.tsx
@@ -6,18 +6,13 @@ import lgZoom from "lightgallery/plugins/zoom";
 import hash from "lightgallery/plugins/hash";
 import pager from "lightgallery/plugins/pager";
 import PhotoThumb from "../components/PhotoThumb";
-
-const fetchPhotos = async () => {
-  const data = await fetch("photos.json")
-    .then((response) => response.json())
-    .catch((error) => {
-      console.error("Error:", error);
-    });
-  return data.photos;
-};
+import photosResource from "../stores/photosResource";
 
 const Photos: Component = () => {
-  const [photos] = createResource(fetchPhotos);
+  const { photos } = photosResource;
+  /**
+   * Initialize light gallery once photos have loaded
+   */
   createEffect(() => {
     if (photos()) {
       let lGallery = lightGallery(document.getElementById("lightgallery"), {
@@ -39,45 +34,6 @@ const Photos: Component = () => {
         <For each={photos()} fallback={<div>Loading...</div>}>
           {(photo) => <PhotoThumb {...photo} />}
         </For>
-
-        {/*
-        <PhotoThumb src="images/photos/_V7A6726.webp" width="400" />
-        <PhotoThumb src="images/photos/_12A1651.webp" width="160" />
-        <PhotoThumb src="images/photos/_12A3178-bw.webp" width="380" />
-        <PhotoThumb src="images/photos/_12A0355.webp" width="360" />
-        <PhotoThumb src="images/photos/_12A1181.webp" width="170" />
-        <PhotoThumb src="images/photos/_12A1211.webp" width="450" />
-        <PhotoThumb src="images/photos/_12A1252.webp" width="450" />
-        <PhotoThumb src="images/photos/_12A4645.webp" width="120" />
-        <PhotoThumb src="images/photos/_12A0202.webp" width="450" />
-        <PhotoThumb src="images/photos/_12A1337.webp" width="430" />
-        <PhotoThumb src="images/photos/_12A1517.webp" width="350" />
-        <PhotoThumb src="images/photos/_12A1572.webp" width="300" />
-        <PhotoThumb src="images/photos/_12A4517.webp" width="180" />
-        <PhotoThumb src="images/photos/_12A1984.webp" width="300" />
-        <PhotoThumb src="images/photos/_12A2781.webp" width="390" />
-        <PhotoThumb src="images/photos/_12A2887.webp" width="300" />
-        <PhotoThumb src="images/photos/_12A3310.webp" width="350" />
-        <PhotoThumb src="images/photos/_12A2850.webp" width="190" />
-        <PhotoThumb src="images/photos/_12A3466.webp" width="350" />
-        <PhotoThumb src="images/photos/_12A3492.webp" width="230" />
-        <PhotoThumb src="images/photos/_12A4484.webp" width="350" />
-        <PhotoThumb src="images/photos/_12A4822.webp" width="220" />
-        <PhotoThumb src="images/photos/_12A4954.webp" width="350" />
-        <PhotoThumb src="images/photos/_12A6225.webp" width="210" />
-        <PhotoThumb src="images/photos/_MG_2735.webp" width="350" />
-        <PhotoThumb src="images/photos/_MG_2989.webp" width="350" />
-        <PhotoThumb src="images/photos/_MG_6302.webp" width="200" />
-        <PhotoThumb src="images/photos/_12A4990.webp" width="200" />
-        <PhotoThumb src="images/photos/_V7A6551.webp" width="350" />
-        <PhotoThumb src="images/photos/colosseum_night.jpg" width="350" />
-        <PhotoThumb src="images/photos/_12A4600.webp" width="180" />
-        <PhotoThumb src="images/photos/IMG_526.jpeg" width="400" />
-        <PhotoThumb src="images/photos/IMG_3197.webp" width="200" />
-        <PhotoThumb src="images/photos/IMG_3738.webp" width="450" />
-        <PhotoThumb src="images/photos/_12A4679.webp" width="220" />
-        <PhotoThumb src="images/photos/IMG_4003.jpg" width="450" />
-        */}
       </div>
     </section>
   );
