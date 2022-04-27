b49ce5034439a61346c1ec5f15b818e00f683de7
Rich Hauck <97491179+rdhauck@users.noreply.github.com>
2022-04-27 15:54:43 -0400

    Moved photos loading logic to external resource

---
+++ src/stores/photosResource.tsx
@@ -0,0 +1,25 @@
+import { createRoot, createResource } from "solid-js";
+/**
+ * Retreives list of photos.
+ * @returns object
+ */
+const fetchPhotos = async () => {
+  const data = await fetch("photos.json")
+    .then((response) => response.json())
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+  return data.photos;
+};
+
+function createPhotos() {
+  const [photos] = createResource(fetchPhotos);
+  const getRandomPhoto = () => {
+    const widePics = photos().filter((photo) => photo.width > 300);
+    const rNum = Math.floor(Math.random() * widePics.length); // only get images with large width
+    return widePics[rNum];
+  };
+  return { photos, getRandomPhoto };
+}
+
+export default createRoot(createPhotos);
